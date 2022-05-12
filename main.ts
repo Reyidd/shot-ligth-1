19ab8d56677e9d49d8e0b5dbb7003303b43ff231
Reyidd <105436475+Reyidd@users.noreply.github.com>
2022-05-12 08:18:57 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    if (sprite.get(LedSpriteProperty.X) == 2) {
+        game.addScore(1)
+    } else {
+        game.gameOver()
+    }
+})
+let sprite: game.LedSprite = null
+sprite = game.createSprite(2, 2)
+let lifes = 5
+basic.forever(function () {
+    sprite.move(1)
+    sprite.ifOnEdgeBounce()
+    basic.pause(200)
+})
